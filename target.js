e3c1326a3f9eff34654e78c080f8de8ace7d7661
J27mendez <mendes_j27@hotmail.com>
2025-01-14 15:37:23 -0300

    feat: percentual de representação por estado da 4 questão

--- target.js
+++ target.js
@@ -95,3 +95,31 @@ function isFibonacci(num) {
 const numero = 21;
 
 console.log(isFibonacci(numero));
+
+/*4 questão*/
+const faturamento = {
+  SP: 67836.43,
+  RJ: 36678.66,
+  MG: 29229.88,
+  ES: 27165.48,
+  Outros: 19849.53,
+};
+
+const totalFaturamento = Object.values(faturamento).reduce(
+  (acc, valor) => acc + valor,
+  0
+);
+
+//representação para cada estado
+const percentuais = {};
+for (const estado in faturamento) {
+  percentuais[estado] = (
+    (faturamento[estado] / totalFaturamento) *
+    100
+  ).toFixed(2);
+}
+
+console.log("Percentual de representação por estado:");
+for (const estado in percentuais) {
+  console.log(`${estado}: ${percentuais[estado]}%`);
+}
